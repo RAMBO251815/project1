4bb0844d16057e2d346d75f097f11755ddb9dd9d
RAMBO251815 <65913558+RAMBO251815@users.noreply.github.com>
2021-07-13 20:59:09 +0700

    ลอง

---
+++ resources/views/test.blade.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>ระบบฐานข้อมูลปริญญานิพนธ์ </title>
+    <!-- Fonts -->
+    <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
+    <link rel="icon" type="image/png" href="images/favicon.png" />
+    <link href="https://fonts.googleapis.com/css?family=Work+Sans:400,600&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="icofont.min.css">
+    <link href="css/bootstrap.min.css" rel="stylesheet" />
+    <link href="css/slick.css" rel="stylesheet" />
+
+    <link href="css/main.css" rel="stylesheet" />
+    <!-- Styles -->
+    <style>
+        .navbar {
+            padding: 25px 0 0 0 !important;
+        }
+    </style>
+</head>
+    <header id="home">
+        <div class="container-fluid">
+            <nav class="navbar navbar-expand-lg navbar-light ">
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                    @if (Route::has('login'))
+                    <div class="navbar-nav ml-auto">
+                        @auth
+                        <a href="{{ url('/home') }}" class="text-sm text-gray-700 underline">Home</a>
+                        @else
+                        <a href="{{ route('login') }}" class="text-sm text-gray-700 underline">Log in</a>
+
+                        @if (Route::has('register'))
+                        <a href="{{ route('register') }}" class="ml-4 text-sm text-gray-700 underline">Register</a>
+                        @endif
+                        @endauth
+                    </div>
+                    @endif
+                </div>
+            </nav>
+        </div>
+        <!-- HERO SECTION -->
+        
+    </header>
+</html>
